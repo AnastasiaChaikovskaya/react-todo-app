9e0d80754e3d9295a9ae9201474a9c16039a8303
chaikovskyiVlad <vladyslav.chaikovskyi@wesoftyou.com>
2024-06-17 00:24:21 +0300

    feat(todo): code refactor

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,7 +1,7 @@
 import { useTodos } from '@/hooks/useTodos';
 import Todo from '@/modules/shared/Todo/Todo';
 import useTodosStore from '@/store/TodosStore';
-import { Frown, Loader, PlusIcon, SquareCheckBig } from 'lucide-react';
+import { Frown, PlusIcon, SquareCheckBig } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import AddTodoModal from '@/modules/shared/Todo/AddTodoModal';
 import { useModal } from '@/hooks/useModal';
@@ -9,6 +9,7 @@ import { AddToDoButton } from '@/modules/shared/Todo/AddTodoButton';
 import EditTodoModal from '@/modules/shared/Todo/EditTodoModal';
 import DeleteModal from '@/modules/shared/Todo/DeleteModal';
 import MoreInfoModal from '@/modules/shared/Todo/MoreInfoModal/MoreInfoModal';
+import { Skeleton } from '@/components/ui/skeleton';
 
 function TodosPage() {
   const { isOpen, openModal } = useModal('add-to-do');
@@ -22,17 +23,26 @@ function TodosPage() {
 
   const hasTodos = toDoList.length > 0;
 
-  if (isLoading) {
-    return (
-      <div className="w-full h-screen flex justify-center items-center">
-        <Loader className="w-12 h-12 animate-spin" />
+  return (
+    <>
+      <div className="flex gap-2 items-center">
+        <SquareCheckBig className=" relative top-1 h-6 w-6 text-stone-950 md:h-10 md:w-10" />
+        <h1 className="text-2xl md:text-5xl text-stone-950 font-bold">Your tasks</h1>
       </div>
-    );
-  }
-
-  if (!hasTodos) {
-    return (
-      <>
+      {isLoading && !hasTodos && (
+        <div className="grid grid-cols-1 gap-y-5 self-center md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 lg:gap-x-6">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <Skeleton key={index} className="w-[300px] h-[300px] bg-stone-300" />
+          ))}
+        </div>
+      )}
+      {hasTodos && !isLoading && (
+        <div className="grid grid-cols-1 gap-y-5 self-center md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 lg:gap-x-6">
+          <AddToDoButton onClick={openModal} />
+          {hasTodos && toDoList.map((todo) => <Todo todo={todo} key={todo._id} />)}
+        </div>
+      )}
+      {!hasTodos && !isLoading && (
         <div className="flex flex-col h-80 items-center justify-center gap-4">
           <Frown className="w-10 h-10" />
           <h1 className="text-3xl font-bold">You don't have any tasks now</h1>
@@ -41,24 +51,7 @@ function TodosPage() {
             Add new To Do
           </Button>
         </div>
-        {isOpen && <AddTodoModal />}
-        {isEditOpen && <EditTodoModal />}
-        {isDeleteOpen && <DeleteModal />}
-        {isMoreInfoOpen && <MoreInfoModal />}
-      </>
-    );
-  }
-
-  return (
-    <>
-      <div className="flex gap-2 items-center">
-        <SquareCheckBig className=" relative top-1 h-6 w-6 text-stone-950 md:h-10 md:w-10" />
-        <h1 className="text-2xl md:text-5xl text-stone-950 font-bold">Your tasks</h1>
-      </div>
-      <div className="grid grid-cols-1 gap-y-5 self-center md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 lg:gap-x-6">
-        <AddToDoButton onClick={openModal} />
-        {hasTodos && toDoList.map((todo) => <Todo todo={todo} key={todo._id} />)}
-      </div>
+      )}
       {isOpen && <AddTodoModal />}
       {isEditOpen && <EditTodoModal />}
       {isDeleteOpen && <DeleteModal />}
